cf4e42f10aa1dc0327322f0047ac403d728e9495
Kanishk Kumar <106447731+Kan201202@users.noreply.github.com>
2026-08-04 00:33:57 +0530

    Add: stone-game-iii - submission-0

---
+++ Data Structures & Algorithms/stone-game-iii/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int> dp(n + 3, 0); 
+        
+        for (int i = n - 1; i >= 0; i--) {
+            int take = 0;
+            dp[i] = -1e9; 
+            
+            for (int j = 0; j < 3 && i + j < n; j++) {
+                take += stoneValue[i + j];
+                dp[i] = max(dp[i], take - dp[i + j + 1]);
+            }
+        }
+        
+        if (dp[0] > 0) return "Alice";
+        if (dp[0] < 0) return "Bob";
+        return "Tie";
+    }
+};
